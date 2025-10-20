304ab4a1296d6d8c95e1f4e0bd72dd1bbbe5568e
yooze <vaogen58@gmail.com>
2025-10-20 19:22:57 +0300

    Add files via upload
    zadanie 1

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    internal class Person
+    {
+        public string name;
+        public int age;
+
+        public void SayHello(string name, int age)
+        {
+            if (age < 0 || age > 120)
+            { 
+                Console.WriteLine($"Ошибка: введите корректный возраст"); 
+            }
+            else
+            {
+                Console.WriteLine($"Привет, я {name}, мне {age} лет!");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.name = "Маша";
+            person.age = 25;
+            person.SayHello(person.name, person.age);
+        }
+    }
+}
