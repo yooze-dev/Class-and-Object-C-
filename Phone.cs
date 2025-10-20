27b81fc02a21e79c44e34db41ea195bf6aabc656
yooze <vaogen58@gmail.com>
2025-10-20 19:24:37 +0300

    Add files via upload
    zadanie 2

---
+++ Phone.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadanie_2
+{
+    internal class Phone
+    {
+        public string brand;
+        public int batteryLevel;
+
+        public void UsePhone(string brand, int batteryLevel)
+        {
+            if (batteryLevel > 100 || batteryLevel < 0)
+            {
+                Console.WriteLine("Ошибка: Введите корректный заряд");
+            }
+            else
+            {
+                if (batteryLevel - 10 < 0)
+                {
+                    Console.WriteLine($"Телефон {brand}, заряд: {0}%");
+                }
+                else
+                {
+                    Console.WriteLine($"Телефон {brand}, заряд: {batteryLevel - 10}%");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,16 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zadanie_1
+namespace zadanie_2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Person person = new Person();
-            person.name = "Маша";
-            person.age = 25;
-            person.SayHello(person.name, person.age);
+            Phone phone = new Phone();
+            phone.brand = "Apple";
+            phone.batteryLevel = 100;
+            phone.UsePhone(phone.brand, phone.batteryLevel);
+            
         }
     }
 }
